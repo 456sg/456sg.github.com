cf16bb9b47df4ea26932dc457389bc21b7a59841
Your Name <you@example.com>
2018-07-09 18:34:36 +0800

    初始化

---
+++ js/index.js
@@ -0,0 +1,14 @@
+// let http=require("http");
+// let fs=require("fs");
+// let server=http.createServer(function(request,response){
+//     response.setHeader("content-type","text/html;charset=utf-8");
+//     fs.readFile("../index.html","utf-8",function (err,data) {  
+//         if(err){
+//             console.log(err);
+//         }else{
+//             response.end(data);
+//         }
+//     })
+    
+// })
+// server.listen(4560)
